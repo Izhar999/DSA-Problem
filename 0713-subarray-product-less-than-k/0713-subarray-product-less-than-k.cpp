f97425bb7f8f01df19fd6bcbd17c1473be715f75
Md Izharul Hassan <izharulhassan7781@gmail.com>
2026-08-25 01:19:29 +0530

    Time: 4 ms (23.33%), Space: 44.7 MB (66.37%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -5,14 +5,15 @@ public:
         int l=0;
         int count=0;
         int pro=1;
+        if(k<=1) return 0;
         for(int r=0;r<n;r++){
             pro=pro*nums[r];
-            while(pro>k){
+            while(pro>=k){
                 pro=pro/nums[l];
                 l++;
             }
             count+=r-l+1;
         }
-        return count+n;
+        return count;
     }
 };
