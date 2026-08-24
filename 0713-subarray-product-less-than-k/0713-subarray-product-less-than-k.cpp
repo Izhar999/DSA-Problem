1b82832131a6e07f0bca8c8c06cada89e9732df1
Md Izharul Hassan <izharulhassan7781@gmail.com>
2026-08-25 01:16:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n=nums.size();
+        int l=0;
+        int count=0;
+        int pro=1;
+        for(int r=0;r<n;r++){
+            pro=pro*nums[r];
+            while(pro>k){
+                pro=pro/nums[l];
+                l++;
+            }
+            count+=r-l+1;
+        }
+        return count+n;
+    }
+};
