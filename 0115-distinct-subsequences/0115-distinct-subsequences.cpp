a5d52cdbc8fe4350b999093455ca53ad193cbc0e
Md Izharul Hassan <izharulhassan7781@gmail.com>
2026-09-07 01:03:59 +0530

    Time: 42 ms (12.83%), Space: 17.5 MB (77.69%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    typedef unsigned long long ull;
+    ull dp[1001][1001];
+    ull solve(string &s,string &t,int i,int j){
+        if(j==t.size()) return dp[i][j]=1;
+        if(i==s.size()) return dp[i][j]=0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==t[j]){
+            return dp[i][j]=solve(s,t,i+1,j+1)+solve(s,t,i+1,j);
+        }
+        else{
+            return dp[i][j]=solve(s,t,i+1,j);
+        }
+        return -1;
+    }
+    int numDistinct(string s, string t) {
+        memset(dp,-1,sizeof(dp));
+        return solve(s,t,0,0);
+    }
+};
