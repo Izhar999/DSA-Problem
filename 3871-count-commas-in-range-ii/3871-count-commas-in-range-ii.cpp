4070d2da38b2945487e9368fa66bf1e00ffffad1
Md Izharul Hassan <izharulhassan7781@gmail.com>
2026-09-10 00:15:33 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.33%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,14 @@
+#define ll long long
+class Solution {
+public:
+    long long countCommas(long long n) {
+        ll result=0;
+        ll st=1000;
+
+        while(st<=n){
+            result+=(n-st+1);
+            st*=1000;
+        }
+        return result;
+    }
+};
